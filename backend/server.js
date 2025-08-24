3464b0ac88b59e8d86d041fa7b448e25adcbd3e3
Your Name <pasapera279@gmail.com>
2025-08-24 13:52:30 -0500

    solving errors to connection fullstack

--- backend/server.js
+++ backend/server.js
@@ -6,24 +6,26 @@ const rateLimit = require('express-rate-limit');
 require('dotenv').config();
 const { connectDB } = require('./config/db');
 const router = require('./routers');
-  
+
 const app = express();
 const PORT = process.env.PORT || 57126;
 
 app.use(helmet());
-app.use(express.json({limit: "500kb"}));
-app.use(cookieParser()); 
+app.use(express.json({ limit: "500kb" }));
+app.use(cookieParser());
 
 const allowedOrigins = [
-  process.env.CLIENT_URL,     
-  process.env.DEV_CLIENT_URL   
+  process.env.CLIENT_URL,
+  process.env.DEV_CLIENT_URL
 ];
 
 app.use(cors({
-  origin: function(origin, callback) {
+  origin: function (origin, callback) {
+    console.log("Petición desde:", origin); // Debug
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
+      console.error("🚨 Not allowed by CORS", origin);
       callback(new Error("Not allowed by CORS"));
     }
   },
@@ -32,8 +34,19 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
-const authLimiter = rateLimit({windowMs: 15*60*1000, max: 100, standardHTTPHeaders: true, legacyHeaders: false});
+// Rate limiter para proteger auth
+const authLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false
+});
+
 connectDB();
+
+app.get("/", (req, res) => {
+  res.json({ message: "Backend funcionando 🚀" });
+});
 
 app.use("/auth", authLimiter);
 app.use("/", authLimiter, router);
@@ -41,4 +54,3 @@ app.use("/", authLimiter, router);
 app.listen(PORT, () => {
   console.log(`✅ Server is running on port ${PORT}`);
 });
-
