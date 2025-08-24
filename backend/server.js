83388070189d11da55891876d8e6d6bcacab8b8f
Your Name <pasapera279@gmail.com>
2025-08-24 12:42:07 -0500

    server updowload

--- backend/server.js
+++ backend/server.js
@@ -14,10 +14,21 @@ app.use(helmet());
 app.use(express.json({limit: "500kb"}));
 app.use(cookieParser()); 
 
+const allowedOrigins = [
+  process.env.CLIENT_URL,     
+  process.env.DEV_CLIENT_URL   
+];
+
 app.use(cors({
-  origin: process.env.CLIENT_URL, //  poner tu frontend (ej: "http://localhost:5173")
-  credentials: true,     
-  methods:['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  origin: function(origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
